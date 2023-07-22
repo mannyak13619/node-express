260b2e9e3c1bed8c814afa552192b1fe5a9ac0aa
bryannavarr <bnrios08@gmail.com>
2023-07-22 07:41:10 -0700

    configured database with sequelize, updated npm packages

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,42 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+try {
+  if (process.env.JAWSDB_URL) { 
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+        operationsAliases: false,
+        logging: function (e) {
+          console.log(e);
+        },
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+        dialectOptions: {
+          socketPath: "/tmp/mysql.sock",
+        },
+        define: {
+          paranoid: true,
+        },
+      }
+    );
+  }
+} catch (e) {
+  console.log("FAILEd:");
+  console.log(e);
 }
 
 module.exports = sequelize;
